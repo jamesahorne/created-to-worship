c02197b0c829b8677142a8160895eaf7722a08c5
James Horne <james99h39@gmail.com>
2020-06-24 15:46:16 +0100

    ✅♻️ Add AppHeader

---
+++ frontend/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <ul>
+      <li>Nav</li>
+      <li>Item</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+}
+</script>
+
+<style scoped>
+nav {
+  padding: 2rem 4rem;
+}
+ul {
+  display: flex;
+  justify-content: flex-end;
+}
+
+li {
+  display: block;
+  margin-right: 2rem;
+}
+li:last-child {
+  margin-right: 0;
+}
+</style>
