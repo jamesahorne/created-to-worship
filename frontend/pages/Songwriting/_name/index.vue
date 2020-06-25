848203fcdba0918c01f9c9a455a50f16eeb31d85
James Horne <james99h39@gmail.com>
2020-06-25 16:07:01 +0100

    ✅ Add Songwriting and songwriting/_name

---
+++ frontend/pages/Songwriting/_name/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1>My Life is My Worship</h1>
+    <Info />
+    <Video />
+  </div>
+</template>
+
+<script>
+import Info from '../../../components/Info'
+import Video from '../../../components/Video'
+
+export default {
+  name: 'Song',
+  components: {
+    Info,
+    Video,
+  },
+}
+</script>
---
+++ frontend/pages/Songwriting/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <h1>Songwriting</h1>
+    <Info />
+    <ul class="videos">
+      <li>
+        <Video />
+      </li>
+      <li>
+        <Video />
+      </li>
+      <li>
+        <Video />
+      </li>
+      <li>
+        <Video />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Info from '../../components/Info'
+import Video from '../../components/Video'
+
+export default {
+  name: 'Songwriting',
+  components: {
+    Info,
+    Video,
+  },
+}
+</script>
+
+<style scoped>
+.videos {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+}
+</style>
