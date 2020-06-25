cc3b6a415ada2961fa4e77bc041feca8b290ef22
James Horne <james99h39@gmail.com>
2020-06-25 16:06:27 +0100

    ♻️ Update Home

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,38 +1,29 @@
 <template>
   <div class="container">
-    hey
+    <Hero />
+    <Type />
+    <Type />
+    <Type />
+    <Type />
   </div>
 </template>
 
 <script>
-export default {}
+import Hero from '../components/Hero'
+import Type from '../components/Type'
+
+export default {
+  name: 'Home',
+  components: {
+    Hero,
+    Type,
+  },
+}
 </script>
 
 <style>
 .container {
   margin: 0 auto;
   max-width: 48rem;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
